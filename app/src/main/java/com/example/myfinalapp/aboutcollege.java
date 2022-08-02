63c4be5c2bf88be138a14c253e63bee6110907c7
Reja dahal <dahalbandana6@gmail.com>
2022-08-02 12:24:58 +0000

    Added all

---
+++ app/src/main/java/com/example/myfinalapp/aboutcollege.java
@@ -0,0 +1,14 @@
+package com.example.myfinalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aboutcollege extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutcollege);
+    }
+}
